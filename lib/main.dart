b3853310f64148dd0e85753d73819929b9630706
blogscn <1470758149@qq.com>
2019-06-04 08:03:49 +0800

    提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,29 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(ShowDialog());
+void main() => runApp(MyApp('Hello World'));
 
-class ShowDialog extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  String content;
+
+  MyApp(this.content);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.blueAccent,
-      ),
-      home: Scaffold(
-        appBar: new AppBar(title: new Text('Show Dialog')),
-        body: Builder(
-          builder: (context) {
-            return RaisedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => CupertinoAlertDialog(
-                        title: Text('CupertinoAlertDialog'),
-                        content: Text('This is a CupertinoAlertDialog'),
-                        actions: <Widget>[
-                          CupertinoDialogAction(
-                            child: Text('OK'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          CupertinoDialogAction(
-                            child: Text('Cancel'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      ),
-
-                  // ======================================================
-                  /*  builder: (context) => AlertDialog(
-                        title: Text('AlertDialog'),
-                        content: SingleChildScrollView(
-                          child: ListBody(
-                            children: <Widget>[
-                              Text('This is an alert dialog'),
-                              Text('add two options.'),
-                            ],
-                          ),
-                        ),
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text('Ok'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          FlatButton(
-                            child: Text('Cancel'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      ), */
-
-// =====================================================
-
-/*                   builder: (context) => SimpleDialog(
-                        title: Text('Simple Dialog Demo'),
-                        // backgroundColor: Colors.blue[300],
-                        children: <Widget>[
-                          SimpleDialogOption(
-                            child: Text('OK'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          SimpleDialogOption(
-                            child: Text('Cancel'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      ), */
-                );
-              },
-              child: Text('Show Dialog'),
-            );
-          },
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-/* class ShowAboutDialogWidge extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  void increment() {
+    setState(() {
+      widget.content += 'dddddddd';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Paint paint = Paint();
+    paint.color = Colors.green;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -106,21 +31,18 @@ class ShowDialog extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter UI Widget -- 对话框'),
+          title: Text("Box Constraints"),
         ),
-        body: Builder(
-          builder: (context) => RaisedButton(
-                onPressed: () {
-                  showAboutDialog(
-                    context: context,
-                    applicationName: 'Flutter UI Widget -- 对话框',
-                    applicationVersion: '1.0.0',
-                  );
-                },
-                child: Text('RaisedButton'),
-              ),
+        body: Container(
+          color: Colors.red,
+          child: Text(
+            'HelloWorld',
+            style: TextStyle(
+              background: paint,
+            ),
+          ),
         ),
       ),
     );
   }
-} */
+}

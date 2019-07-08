da00da66cdb7d531591d472433bbf240fa42eff2
taocui <taocui@wisedu.com>
2019-07-08 18:05:09 +0800

    文字样式

--- lib/views/TextPage.dart
+++ lib/views/TextPage.dart
@@ -25,7 +25,48 @@ class TextRoute extends StatelessWidget {
           Text(
             text.toUpperCase(),
             textScaleFactor: 1.5,
-          )
+          ),
+          Text(
+            text,
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18.0,
+                height: 1.2,
+                fontFamily: 'Courier',
+                background: new Paint()..color = Colors.yellow,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed),
+          ),
+          Text.rich(TextSpan(children: [
+            TextSpan(
+              text: 'Home：',
+            ),
+            TextSpan(
+              text: "https://flutterchina.club",
+              style: TextStyle(color: Colors.blue),
+            ),
+          ])),
+          DefaultTextStyle(
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+            ),
+            textAlign: TextAlign.start,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("hello world"),
+                Text("I am Jack"),
+                Text(
+                  "I am Jack",
+                  style: TextStyle(
+                    inherit: false, //不继承样式
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
